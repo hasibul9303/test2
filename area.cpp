0c51ab8766973db429307dca113ce08831a50dd8
Restyled.io <commits@restyled.io>
2020-06-28 14:09:49 +0000

    Restyled by clang-format

--- area.cpp
+++ area.cpp
@@ -1,13 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-main()
-{
-    system("COLOR F0");
-    //Area of triangle
-    float area,h,b;
-    printf("Enter the height and base\t");
-    scanf("%f,%f",&h,&b);
-    area= (h*b)/2;
-    printf("\nArea= %f",area);
+#include <stdio.h>
+#include <stdlib.h>
+main() {
+  system("COLOR F0");
+  // Area of triangle
+  float area, h, b;
+  printf("Enter the height and base\t");
+  scanf("%f,%f", &h, &b);
+  area = (h * b) / 2;
+  printf("\nArea= %f", area);
 }
-
