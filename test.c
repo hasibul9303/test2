6bfa60218706fc32dc995614402c1cb20b270c7d
pritiadhikary <91726072+pritiadhikary@users.noreply.github.com>
2021-10-15 02:59:24 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+int a,b,c;
+printf("enter three numbers");
+scanf("%d%d%d",&a,&b,&c);
+if (a>b && a>c)
+printf("maximum number is a =%d",a);
+else if (b>a && b>c)
+printf("maximum number is a =%d",b);
+else
+printf("maximum number is a =%d",c);
+if(a<b && a<c)
+printf("minimum number is a =%d",a);
+else if(b<a && b<c)
+printf("minimum number is a =%d",b);
+else
+printf("minimum number is a =%d",c);
+}
