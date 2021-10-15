4847352be8631313dfc47178f3c4678dae3e157f
Restyled.io <commits@restyled.io>
2021-10-15 04:49:37 +0000

    Restyled by clang-format

--- Ninad.c
+++ Ninad.c
@@ -5,63 +5,54 @@
 
 // A structure to represent a stack
 struct StackNode {
-    int data;
-    struct StackNode* next;
+  int data;
+  struct StackNode *next;
 };
 
-struct StackNode* newNode(int data)
-{
-    struct StackNode* stackNode =
-        (struct StackNode*)
-        malloc(sizeof(struct StackNode));
-    stackNode->data = data;
-    stackNode->next = NULL;
-    return stackNode;
+struct StackNode *newNode(int data) {
+  struct StackNode *stackNode =
+      (struct StackNode *)malloc(sizeof(struct StackNode));
+  stackNode->data = data;
+  stackNode->next = NULL;
+  return stackNode;
 }
 
-int isEmpty(struct StackNode* root)
-{
-    return !root;
+int isEmpty(struct StackNode *root) { return !root; }
+
+void push(struct StackNode **root, int data) {
+  struct StackNode *stackNode = newNode(data);
+  stackNode->next = *root;
+  *root = stackNode;
+  printf("%d pushed to stack\n", data);
 }
 
-void push(struct StackNode** root, int data)
-{
-    struct StackNode* stackNode = newNode(data);
-    stackNode->next = *root;
-    *root = stackNode;
-    printf("%d pushed to stack\n", data);
+int pop(struct StackNode **root) {
+  if (isEmpty(*root))
+    return INT_MIN;
+  struct StackNode *temp = *root;
+  *root = (*root)->next;
+  int popped = temp->data;
+  free(temp);
+
+  return popped;
 }
 
-int pop(struct StackNode** root)
-{
-    if (isEmpty(*root))
-        return INT_MIN;
-    struct StackNode* temp = *root;
-    *root = (*root)->next;
-    int popped = temp->data;
-    free(temp);
-
-    return popped;
+int peek(struct StackNode *root) {
+  if (isEmpty(root))
+    return INT_MIN;
+  return root->data;
 }
 
-int peek(struct StackNode* root)
-{
-    if (isEmpty(root))
-        return INT_MIN;
-    return root->data;
-}
+int main() {
+  struct StackNode *root = NULL;
 
-int main()
-{
-    struct StackNode* root = NULL;
+  push(&root, 10);
+  push(&root, 20);
+  push(&root, 30);
 
-    push(&root, 10);
-    push(&root, 20);
-    push(&root, 30);
+  printf("%d popped from stack\n", pop(&root));
 
-    printf("%d popped from stack\n", pop(&root));
+  printf("Top element is %d\n", peek(root));
 
-    printf("Top element is %d\n", peek(root));
-
-    return 0;
+  return 0;
 }
